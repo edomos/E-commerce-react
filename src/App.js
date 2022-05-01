e5765137424418d574c35b0593d32153734fbca2
edomos <alf.ramoz@gmail.com>
2022-05-01 00:10:58 -0700

    create books page with sorting options

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import Landing from "./components/Landing";
 import Nav from "./components/Nav"
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data"
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={ books }/>} />
+          {/* <Route path="/books/:id" element={<BooksInfo books={ books }
+          addToCart= { addToCart } cart={ cart } />} />
+          <Route path="/cart" element={<Cart books={ books } cart={ cart }
+          changeQuantity={changeQuantity } />} /> */}
+          {/* <Footer /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
